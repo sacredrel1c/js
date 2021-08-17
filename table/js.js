223a4c97d6716017a617b829a351d119fa947370
sacredrel1c <tpek07@gmail.com>
2021-08-17 12:11:19 +0300

    complete lvl 1-3-table

---
+++ table/js.js
@@ -0,0 +1,37 @@
+﻿function DataTable(config, data) {
+	let divElem = document.querySelector(config.parent);
+	let table = "<table class='rounded'><thead><tr><th class='number'>№</th>";
+	config.columns.forEach(element => table+="<th class="+element.value+">"+element.title+"</th>");
+	table += "</tr></thead><tbody>";
+	data.forEach(function (row,index){
+		table+="<tr><td>"+(index+1)+"</td>";
+		config.columns.forEach(function (cell){
+			table+="<td>"+row[cell.value]+"</td>";
+		})
+		table+="</tr>";
+	})
+	table += "</tbody></table>";
+	divElem.innerHTML+=table;
+}
+
+
+const config1 = {
+  parent: '#usersTable',
+  columns: [
+    {title: 'Имя', value: 'name'},
+    {title: 'Фамилия', value: 'surname'},
+    {title: 'Возраст', value: 'age'},
+  ]
+};
+
+
+
+const users = [
+  {id: 30050, name: 'Вася', surname: 'Петров', age: 12},
+  {id: 30051, name: 'Вася', surname: 'Васечкин', age: 15},
+];
+window.onload = function (){
+DataTable(config1, users)
+};
+
+
