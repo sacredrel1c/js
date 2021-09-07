8daad8fea296857831721ecd84486c8bdd736355
sacredrel1c <tpek07@gmail.com>
2021-09-07 15:18:15 +0300

    completed lvl 1-4

--- table-1-4/js.js
+++ table-1-4/js.js
@@ -1,4 +1,13 @@
-﻿async function DataTable(config, data = null) {
+﻿/**
+ * This function render a page with data from backend or input from array data. In render process we wait to response from server and then create
+ * structure of body with elements.
+ * Elements have a button with delete function from DB.
+ * @param config - structure of table and api url
+ * @param data - alternative data source.
+ * @returns {Promise<void>} - promise return a data from DB.
+ * @constructor
+ */
+async function DataTable(config, data = null) {
     let divElem = document.querySelector(config.parent);
     divElem.innerHTML = "<button id='addElement' onclick='addElement()'>Добавить</button>";
     let table = "<table class='rounded'><thead><tr><th class='number'>№</th>";
@@ -31,7 +40,11 @@
     divElem.innerHTML += table;
 }
 
-
+/**
+ * Structure of table.
+ * @type {{parent: string, apiUrl: string, columns: [{title: string, value: string},
+ * {title: string, value: string}, {title: string, value: string}, {title: string, value: string}]}}
+ */
 const config1 = {
     parent: '#usersTable',
     columns: [
@@ -40,22 +53,29 @@ const config1 = {
         {title: 'Дата создания', value: 'birthday'},
         {title: 'Аватар', value: 'avatar'},
     ],
-    apiUrl: "https://mock-api.shpp.me/arasputnyjtest/users"
+    apiUrl: "https://mock-api.shpp.me/arasputnyjtest/users/"
 };
 
-
+/**
+ * Some test data to use as input parameter in DataTable function
+ * @type {[{surname: string, name: string, id: number, age: number}, {surname: string, name: string, id: number, age: number}]}
+ */
 const users = [
     {id: 30050, name: 'Вася', surname: 'Петров', age: 12},
     {id: 30051, name: 'Вася', surname: 'Васечкин', age: 15},
 ];
 
+/**
+ * Function to delete User from backend.
+ * @param idToDelete - id number of deletion user.
+ * @returns {Promise<void>} - rerender data table without deleted user.
+ */
 async function deleteUser(idToDelete) {
-    const url = "https://mock-api.shpp.me/arasputnyjtest/users/";
-    const response = await fetch(url+idToDelete,{
-        method:"DELETE"
+    const response = await fetch(config1.apiUrl + idToDelete, {
+        method: "DELETE"
     });
-    if(response.ok){
-        alert("Удалено строку № "+idToDelete);
+    if (response.ok) {
+        alert("Удалено строку № " + idToDelete);
 
         DataTable(config1)
             .then(() => {
@@ -64,6 +84,9 @@ async function deleteUser(idToDelete) {
     }
 }
 
+/**
+ * function add event listeners to all delete buttons.
+ */
 function addClickDeleteListenersToButtons() {
     let button = document.querySelectorAll("button.delete");
     button.forEach(function (currentButton) {
@@ -73,26 +96,79 @@ function addClickDeleteListenersToButtons() {
         });
     });
 }
-function addElement(){
-    let table = document.getElementById('tableBody');
-    let newRow = "<tr><td></td>";
-    config1.columns.forEach(function (column) {
-        newRow += "<td><input class='addElement' name='"+column.value+"'/></td>";
-    });
-    newRow += "<td></td></tr>";
-    table.innerHTML = newRow + table.innerHTML;
-    let inputs = document.querySelectorAll('.addElement');
+
+/**
+ * Simple validator without logic of validation, have one validation rule - required.
+ * @param inputs - input elements to validate.
+ * @returns {boolean} - if validation failed in one more input return false.
+ */
+function checkInputs(inputs) {
+    let valid = true;
     inputs.forEach(function (currentInput) {
-        currentInput.addEventListener('keydown', function (k) {
-            if(k.keyCode === 13){
-                console.dir(this.name);
-            }
-        });
+        if (currentInput.validity.valueMissing) {
+            currentInput.classList.add("invalid");
+            valid = false;
+        } else {
+            currentInput.classList.remove("invalid");
+        }
+    });
+    return valid;
+}
 
+/**
+ * Function sending data to backend, after create an object to send.
+ * @param checkedData - array of validated data to send
+ * @returns {Promise<void>} - rerender data table on the page
+ */
+async function sendData(checkedData) {
+    let dataToSend = {};
+    checkedData.forEach(function (currentInput) {
+        dataToSend[currentInput.name] = currentInput.value;
     })
+    const response = await fetch(config1.apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(dataToSend)
+    });
+    if (response.ok) {
+        DataTable(config1)
+            .then(() => {
+                addClickDeleteListenersToButtons();
+            });
+    }
+}
 
+/**
+ * Function add input elements on web-page if not exists and button "add" has been clicked
+ */
+function addElement() {
+    if (!document.querySelector('.addElement')) {
+        let table = document.getElementById('tableBody');
+        let newRow = "<tr><td></td>";
+        config1.columns.forEach(function (column) {
+            newRow += "<td><input class='addElement' name='" + column.value + "' required/></td>";
+        });
+        newRow += "<td></td></tr>";
+        table.innerHTML = newRow + table.innerHTML;
+        let inputs = document.querySelectorAll('.addElement');
+        inputs.forEach(function (currentInput) {
+            currentInput.addEventListener('keydown', function (k) {
+                if (k.keyCode === 13) {
+
+                    if (checkInputs(inputs)) {
+                        sendData(inputs);
+                    }
+
+                }
+            });
+
+        })
+    }
 
 }
+
 window.onload = function () {
     DataTable(config1)
         .then(() => {
